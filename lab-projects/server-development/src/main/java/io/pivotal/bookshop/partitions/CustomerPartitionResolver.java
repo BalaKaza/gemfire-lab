50b4ab1d46210790d2c539a75a87132a9a170ab3
Bala Kaza <bkazavenkata@pivotal.io>
2019-07-10 12:08:27 -0700

    Lab Work

--- lab-projects/server-development/src/main/java/io/pivotal/bookshop/partitions/CustomerPartitionResolver.java
+++ lab-projects/server-development/src/main/java/io/pivotal/bookshop/partitions/CustomerPartitionResolver.java
@@ -7,27 +7,25 @@ import org.apache.geode.cache.PartitionResolver;
 
 import io.pivotal.bookshop.keys.OrderKey;
 
-public class CustomerPartitionResolver implements PartitionResolver<OrderKey,Object> {
-	private static final long serialVersionUID = 1L;
+public class CustomerPartitionResolver implements PartitionResolver<OrderKey, Object> {
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public void close()
-	{
-		//nothing to do when the Region closes
-	}
-	
-	@Override
-	public String getName()
-	{
-		return this.getClass().getName() + "PartitionResolver";
-	}
-	
-	@Override
-	public Serializable getRoutingObject(EntryOperation<OrderKey,Object> eo)
-	{
-		// TODO-02: Implement getRoutingObject to obtain the customerId portion of the key and return it
-		return null;
-	}
+    @Override
+    public void close() {
+        //nothing to do when the Region closes
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getName() + "PartitionResolver";
+    }
+
+    @Override
+    public Serializable getRoutingObject(EntryOperation<OrderKey, Object> eo) {
+        // TODO-02: Implement getRoutingObject to obtain the customerId portion of the key and return it
+        OrderKey key = eo.getKey();
+        return key.getCustomerNumber();
+    }
 
 
 }
