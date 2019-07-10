919267d7d50f623aa45ed0f5a490971b3c8f81b2
Bala Kaza <bkazavenkata@pivotal.io>
2019-07-10 11:49:25 -0700

    Update with new queries.

--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/CustomerDAO.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/CustomerDAO.java
@@ -1,6 +1,7 @@
 package io.pivotal.bookshop.dao;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.geode.cache.client.ClientCache;
@@ -10,43 +11,52 @@ import org.apache.geode.cache.query.Struct;
 import io.pivotal.bookshop.domain.Customer;
 
 public class CustomerDAO extends DAOCommon<Integer, Customer> {
-	// Note that there is a generic region now defined in the DAOCommon class that you can use called 'region'.
-	// Notice also that type type (key and value) are defined by the types specified above
+    // Note that there is a generic region now defined in the DAOCommon class that you can use called 'region'.
+    // Notice also that type type (key and value) are defined by the types specified above
 
-	public CustomerDAO(ClientCache clientCache) {
-		super(clientCache, "Customer");
-	}
+    public CustomerDAO(ClientCache clientCache) {
+        super(clientCache, "Customer");
+    }
 
-	/**
-	 * Constructs a query string returning all entries in the /Customer region
-	 * 
-	 * @return A list of all Customer entries
-	 */
-	public List<Customer> getAll() {
-		// TODO-02: Implement the getAll() method by by
+    /**
+     * Constructs a query string returning all entries in the /Customer region
+     *
+     * @return A list of all Customer entries
+     */
+    public List<Customer> getAll() {
+        // TODO-02: Implement the getAll() method by by
         //          1) creating the query string and
         //          2) returning the results of calling doQuery but converting
-		//             to a java.util.List
-		String queryString1 = "";
-		return null;
-	}
+        //             to a java.util.List
+        String queryString1 = "SELECT * FROM /Customer";
+        SelectResults<Customer> results = (SelectResults<Customer>) doQuery(queryString1);
+        List<Customer> customers = results.asList();
+        return customers;
+    }
 
-	/**
-	 * Executes a Projection List query having only the customerNumber, firstName and lastName fields
-	 * 
-	 * @return A list of all Customer entries with only the customerNumber, firstName and lastName fields set
-	 */
-	public List<Customer> getAllSummary() {
-		List<Customer> custList = new ArrayList<Customer>();
+    /**
+     * Executes a Projection List query having only the customerNumber, firstName and lastName fields
+     *
+     * @return A list of all Customer entries with only the customerNumber, firstName and lastName fields set
+     */
+    public List<Customer> getAllSummary() {
+        List<Customer> custList = new ArrayList<Customer>();
 
-		// TODO-04: implement the method to select only the customerNumber, firstName and lastName as a projection list, process
-		// the results and return as a list of Customer
-		String queryString1 = "";
-		
-		return custList;
+        // TODO-04: implement the method to select only the customerNumber, firstName and lastName as a projection list, process
+        // the results and return as a list of Customer
+        String queryString1 = "SELECT c.customerNumber, c.firstName, c.lastName FROM /Customer c";
+        SelectResults<Struct> results = (SelectResults<Struct>) doQuery(queryString1);
+        Iterator<Struct> i = results.iterator();
+        while (i.hasNext()) {
+            Struct struct = (Struct) i.next();
+            String firstName = struct.get("firstName").toString();
+            String lastName = struct.get("lastName").toString();
+            Integer customerNumber = (Integer) struct.get("customerNumber");
+            custList.add(new Customer(customerNumber,firstName,lastName));
+        }
+        return custList;
 
-	}
+    }
 
-	
 
 }
--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/DAOCommon.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/DAOCommon.java
@@ -2,46 +2,52 @@ package io.pivotal.bookshop.dao;
 
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
-import org.apache.geode.cache.query.Query;
-import org.apache.geode.cache.query.QueryService;
-import org.apache.geode.cache.query.SelectResults;
+import org.apache.geode.cache.query.*;
 
-public abstract class DAOCommon<K,T> {
-	private ClientCache clientCache;
-	private Region<K, T> region;
-  
+public abstract class DAOCommon<K, T> {
+    private ClientCache clientCache;
+    private Region<K, T> region;
 
-   public DAOCommon(ClientCache clientCache, String regionName) {
-		this.clientCache = clientCache;
-		this.region = this.clientCache.getRegion(regionName);
-	}
 
-	public void doInsert(K key,T entry) {
-		region.create(key, entry);
-	}
-    
+    public DAOCommon(ClientCache clientCache, String regionName) {
+        this.clientCache = clientCache;
+        this.region = this.clientCache.getRegion(regionName);
+    }
+
+    public void doInsert(K key, T entry) {
+        region.create(key, entry);
+    }
+
     public T doGet(K key) {
-    	return region.get(key);
+        return region.get(key);
     }
-    
+
     public void doUpdate(K key, T entry) {
-    	region.put(key, entry);
+        region.put(key, entry);
     }
-    
+
     public void doDelete(K key) {
-    	region.remove(key);
+        region.remove(key);
     }
 
     /**
-     * Uses the supplied queryString to execute a GemFire query using OQL and the GemFire QueryService. This is a very basic 
+     * Uses the supplied queryString to execute a GemFire query using OQL and the GemFire QueryService. This is a very basic
      * implementation that does not take advantage of query parameters or anything like that.
-     * 
+     *
      * @param queryString The string to be passed to the query service
      * @return SelectResults is a collection of any number of types depending on the nature of the query
      */
-	public SelectResults<?> doQuery(String queryString) {
-		// TODO-01: Implement the doQuery method to use the supplied query string to execute, returning the SelectResults
-		// Catch any exceptions and re-throw as a QueryUncheckedException, which is an unchecked exception provided in this package.
-		return null;
-	}    
+    public SelectResults<?> doQuery(String queryString) {
+        // TODO-01: Implement the doQuery method to use the supplied query string to execute, returning the SelectResults
+        // Catch any exceptions and re-throw as a QueryUncheckedException, which is an unchecked exception provided in this package.
+        QueryService queryService = clientCache.getQueryService();
+        Query query = queryService.newQuery(queryString);
+        SelectResults<?> results = null;
+        try {
+            results = (SelectResults<?>) query.execute();
+        } catch (QueryException e) {
+            throw new QueryUncheckedException();
+        }
+        return results;
+    }
 }
