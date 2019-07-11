aa3c7098f52561183a8631761c63ff32be5953d3
Bala Kaza <bkazavenkata@pivotal.io>
2019-07-11 11:58:33 -0700

    Lab Work.

--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/GemFireClientCacheHelper.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/GemFireClientCacheHelper.java
@@ -12,79 +12,76 @@ import io.pivotal.bookshop.keys.OrderKey;
 
 public class GemFireClientCacheHelper {
 
-	/**
-	 * Initializes the ClientCache. It also indirectly initializes the
-	 * BookMaster, Customer and BookOrder regions by calling the associated
-	 * static methods.
-	 * 
-	 * @returnInitialized ClientCache and regions initialized as well.
-	 */
-	public static ClientCache create() {
-		ClientCache cache = new ClientCacheFactory()
-				.set("name", "Client App")
-				.addPoolLocator("localhost", 10334)
-				.setPoolSubscriptionEnabled(true)
-				.create();
+    /**
+     * Initializes the ClientCache. It also indirectly initializes the
+     * BookMaster, Customer and BookOrder regions by calling the associated
+     * static methods.
+     *
+     * @returnInitialized ClientCache and regions initialized as well.
+     */
+    public static ClientCache create() {
+        ClientCache cache = new ClientCacheFactory()
+                .set("name", "Client App")
+                .addPoolLocator("localhost", 10334)
+                .setPoolSubscriptionEnabled(true)
+                .create();
 
-		createProxyRegion(cache,"BookMaster", true);
-		createProxyRegion(cache,"Customer", true);
-		createProxyRegion(cache,"BookOrder", true);
-		return cache;
-	}
+        createProxyRegion(cache, "BookMaster", true);
+        createProxyRegion(cache, "Customer", true);
+        createProxyRegion(cache, "BookOrder", true);
+        return cache;
+    }
 
-	/**
-	 * Initializes the ClientCache, including enabling auto-serialization and
-	 * optionally setting read-serialized to true. It also indirectly
-	 * initializes the BookMaster, Customer and BookOrder regions by calling the
-	 * associated static methods.
-	 * 
-	 * @param readSerialized
-	 *            Boolean indicating whether the client cache should be
-	 *            initialized with read-serialized set to true
-	 * @return Initialized ClientCache and regions initialized as well.
-	 */
-	public static ClientCache createPdxEnabled(boolean readSerialized) {
-		// Initial configuration of the ClientCacheFactory
-		ClientCacheFactory clientFactory = new ClientCacheFactory()
-				.set("name", "Client App")
-				.addPoolLocator("localhost", 10334)
-				.setPoolSubscriptionEnabled(true);
+    /**
+     * Initializes the ClientCache, including enabling auto-serialization and
+     * optionally setting read-serialized to true. It also indirectly
+     * initializes the BookMaster, Customer and BookOrder regions by calling the
+     * associated static methods.
+     *
+     * @param readSerialized Boolean indicating whether the client cache should be
+     *                       initialized with read-serialized set to true
+     * @return Initialized ClientCache and regions initialized as well.
+     */
+    public static ClientCache createPdxEnabled(boolean readSerialized) {
+        // Initial configuration of the ClientCacheFactory
+        ClientCacheFactory clientFactory = new ClientCacheFactory()
+                .set("name", "Client App")
+                .addPoolLocator("localhost", 10334)
+                .setPoolSubscriptionEnabled(true);
 
-		// TODO-02: Add configuration to enable auto-serialization on
-		// the client side (and enforce check portability)
-		
-		// TODO-08: Set read-serialized to true
+        // TODO-02: Add configuration to enable auto-serialization on
+        // the client side (and enforce check portability)
+        clientFactory.setPdxSerializer(new ReflectionBasedAutoSerializer(true, "io.pivotal.bookshop.domain.*"));
 
-		
-		ClientCache cache = clientFactory.create();
-		createProxyRegion(cache,"BookMaster", false);
-		createProxyRegion(cache,"Customer", false);
-		createProxyRegion(cache,"BookOrder", false);
-		
-		return cache;
-	}
+        // TODO-08: Set read-serialized to true
+        clientFactory.setPdxReadSerialized(readSerialized);
 
-	/**
-	 * Initializes a region by name and creates as either caching proxy or proxy
-	 * depending on the boolean flag supplied. Once created, it can later be
-	 * fetched using the getRegion() method call on the ClientCache.
-	 * 
-	 * @param cache
-	 *            ClientCache to use for creating the ClientRegionFactory
-	 * @param regionName
-	 *            Name of the region to initialize
-	 * @param createAsCachingProxy
-	 *            Boolean indicating wither region should be a caching proxy
-	 */
-	private static void createProxyRegion(ClientCache cache, String regionName, boolean createAsCachingProxy) {
-		ClientRegionFactory regionFactory;
-		if (createAsCachingProxy)
-			regionFactory = cache.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
-		else
-			regionFactory = cache.createClientRegionFactory(ClientRegionShortcut.PROXY);
+        ClientCache cache = clientFactory.create();
+        createProxyRegion(cache, "BookMaster", false);
+        createProxyRegion(cache, "Customer", false);
+        createProxyRegion(cache, "BookOrder", false);
 
-		regionFactory.create(regionName);
-	}
+        return cache;
+    }
+
+    /**
+     * Initializes a region by name and creates as either caching proxy or proxy
+     * depending on the boolean flag supplied. Once created, it can later be
+     * fetched using the getRegion() method call on the ClientCache.
+     *
+     * @param cache                ClientCache to use for creating the ClientRegionFactory
+     * @param regionName           Name of the region to initialize
+     * @param createAsCachingProxy Boolean indicating wither region should be a caching proxy
+     */
+    private static void createProxyRegion(ClientCache cache, String regionName, boolean createAsCachingProxy) {
+        ClientRegionFactory regionFactory;
+        if (createAsCachingProxy)
+            regionFactory = cache.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
+        else
+            regionFactory = cache.createClientRegionFactory(ClientRegionShortcut.PROXY);
+
+        regionFactory.create(regionName);
+    }
 
 
 }
--- lab-projects/client-application/src/test/java/io/pivotal/bookshop/tests/pdx/PdxInstanceClientTests.java
+++ lab-projects/client-application/src/test/java/io/pivotal/bookshop/tests/pdx/PdxInstanceClientTests.java
@@ -16,44 +16,43 @@ import io.pivotal.bookshop.domain.Customer;
 
 // TODO-10: Run this test to verify correct configuration and implementation of the CustomerDAO
 public class PdxInstanceClientTests {
-	private ClientCache clientCache;
-	private Region<Integer, Customer> customers;
+    private ClientCache clientCache;
+    private Region<Integer, Customer> customers;
 
-	@Before
-	public void setUp() throws Exception {
-		this.clientCache = GemFireClientCacheHelper.createPdxEnabled(true);
-		customers = clientCache.getRegion("Customer");
-		customers.removeAll(customers.keySetOnServer());
-		populateCustomers();
-	}
+    @Before
+    public void setUp() throws Exception {
+        this.clientCache = GemFireClientCacheHelper.createPdxEnabled(true);
+        customers = clientCache.getRegion("Customer");
+        customers.removeAll(customers.keySetOnServer());
+        populateCustomers();
+    }
 
-	@After
-	public void cleanUp() {
-		clientCache.close();
-	}
-		
-	@Test
-	public void testPdxInstanceConfiguration() {
-		CustomerDAO dao = new CustomerDAO(clientCache);
-		String customerName = dao.getCustomerName(5542);
-		assertNotNull("Failed PdxInstance configuration - PdxInstance not returned", customerName);
-		assertEquals("Improper construction of customer name", "Wax, Lula", customerName);
-	}
+    @After
+    public void cleanUp() {
+        clientCache.close();
+    }
+
+    @Test
+    public void testPdxInstanceConfiguration() {
+        CustomerDAO dao = new CustomerDAO(clientCache);
+        String customerName = dao.getCustomerName(5542);
+        assertNotNull("Failed PdxInstance configuration - PdxInstance not returned", customerName);
+        assertEquals("Improper construction of customer name", "Wax, Lula", customerName);
+    }
 
 
-	public void populateCustomers()
-	{		
-		
-		Customer cust1 = new Customer(5598, "Kari", "Powell", ""+44444);
-		cust1.addOrder(17600);
-		customers.put(cust1.getCustomerNumber(), cust1);
-		
-		Customer cust2 = new Customer (5542, "Lula", "Wax", ""+12345);
-		customers.put(cust2.getCustomerNumber(), cust2);
-		
-		Customer cust3 = new Customer (6024, "Trenton", "Garcia", ""+88888);
-		cust3.addOrder(17700);
-		customers.put(cust3.getCustomerNumber(), cust3);
-	}
+    public void populateCustomers() {
+
+        Customer cust1 = new Customer(5598, "Kari", "Powell", "" + 44444);
+        cust1.addOrder(17600);
+        customers.put(cust1.getCustomerNumber(), cust1);
+
+        Customer cust2 = new Customer(5542, "Lula", "Wax", "" + 12345);
+        customers.put(cust2.getCustomerNumber(), cust2);
+
+        Customer cust3 = new Customer(6024, "Trenton", "Garcia", "" + 88888);
+        cust3.addOrder(17700);
+        customers.put(cust3.getCustomerNumber(), cust3);
+    }
 
 }
