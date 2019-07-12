aa5f0103dc135fe131070ab62d6619c5aad9aab8
Bala Kaza <bkazavenkata@pivotal.io>
2019-07-12 09:33:18 -0700

    lab work

--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/function/FunctionExecutor.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/function/FunctionExecutor.java
@@ -1,23 +1,34 @@
 package io.pivotal.bookshop.function;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.Pool;
+import org.apache.geode.cache.client.PoolManager;
 import org.apache.geode.cache.execute.Execution;
 import org.apache.geode.cache.execute.FunctionService;
 import org.apache.geode.cache.execute.ResultCollector;
 
 public class FunctionExecutor {
 
-	// TODO-05: Inspect the signature of this method
-	public static BigDecimal callSumFunction(Region region, String fieldName) {
-		// TODO-06: execute the function using the supplied 'fieldName' for the field and the supplied region to execute the function on
+    // TODO-05: Inspect the signature of this method
+    public static BigDecimal callSumFunction(Region region, String fieldName) {
+        // TODO-06: execute the function using the supplied 'fieldName' for the field and the supplied region to execute the function on
+        Pool pool = PoolManager.find("DEFAULT");
+        Execution execution = FunctionService.onServers(pool);
+
+        execution.withCollector(new SummingResultCollector());
+        execution.setArguments(fieldName);
+        
+        ResultCollector resultCollector = execution.execute("GenericSumFunction");
+        List result = (List) resultCollector.getResult();
 
 
+        // TODO-07: Get result and assert that the two orders total $93.95
 
-		// TODO-07: Get result and assert that the two orders total $93.95
+        BigDecimal finalResult = (BigDecimal) result.get(0);
+        return finalResult;
 
-		return null;
-	
-	}
+    }
 }
--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/function/SummingResultCollector.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/function/SummingResultCollector.java
@@ -6,47 +6,55 @@ import org.apache.geode.distributed.DistributedMember;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Float.*;
 
 public class SummingResultCollector implements ResultCollector<Serializable, Serializable> {
 
-	  // TODO-01: Determine how you will contain results
-	  //    HINT: You only need to keep a final sum
-	  
+    // TODO-01: Determine how you will contain results
+    //    HINT: You only need to keep a final sum
 
-	  @Override
-	  public void addResult(DistributedMember memberID,
-	                        Serializable resultOfSingleExecution) {
-	    // TODO-02: Implement the addResult method
-	    //    HINT: Keep in mind what was sent from the function in the prior lab.
-	   
-	  }
+    BigDecimal result = BigDecimal.ZERO;
 
-	  @Override
-	  public void clearResults() {
-	    // TODO-03: Implement clearResults method
-	    
+    @Override
+    public void addResult(DistributedMember memberID,
+                          Serializable resultOfSingleExecution) {
+        // TODO-02: Implement the addResult method
+        //    HINT: Keep in mind what was sent from the function in the prior lab.
+        String s = resultOfSingleExecution.toString();
+        BigDecimal res = BigDecimal.valueOf(parseFloat(s));
+        MathContext mathContext = new MathContext(2, RoundingMode.HALF_UP);
+        result = res.round(mathContext);
+    }
 
-	  }
+    @Override
+    public void clearResults() {
+        // TODO-03: Implement clearResults method
+        result = BigDecimal.ZERO;
 
-	  @Override
-	  public void endResults() {
-	    // No special processing required.
+    }
 
-	  }
+    @Override
+    public void endResults() {
+        // No special processing required.
 
-	  @Override
-	  public Serializable getResult() throws FunctionException {
-	    // TODO-04: Implement getResult method
-	    return null;
-	  }
+    }
 
-	  @Override
-	  public Serializable getResult(long timeout, TimeUnit unit)
-	      throws FunctionException, InterruptedException {
-	    // This method will do whatever the other getResult() method does.
-	    return this.getResult();
-	  }
+    @Override
+    public Serializable getResult() throws FunctionException {
+        // TODO-04: Implement getResult method
+        return result;
+    }
 
-	}
+    @Override
+    public Serializable getResult(long timeout, TimeUnit unit)
+            throws FunctionException, InterruptedException {
+        // This method will do whatever the other getResult() method does.
+        return this.getResult();
+    }
+
+}
 
